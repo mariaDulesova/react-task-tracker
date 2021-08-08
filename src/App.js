76826c31ecd2c5a192f6268c3427b2cca8dc90e4
mariaDulesova <maria.dulesova@gmail.com>
2021-08-08 15:28:05 +0200

    Add Task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import {useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+
+  const [showAddTask, setShowAddTask] =useState(false)
+
   const[tasks, setTasks]=useState([
     //use State is immutable and can not be changed. You can't push elements inside. But you can use setTasks ([...])
     {
@@ -18,11 +22,34 @@ function App() {
         reminder: true
     }
   ])
+
+  //Delete Task
+  const deleteTask =(id) => {
+    setTasks(tasks.filter(
+      (task)=> task.id !==id
+    ))
+  }
+
+  //Add Task
+  const addTask =(task) => {
+    const id = Math.floor(Math.random() *10000)+1  
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+  
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map(
+      task => task.id === id ? { ...task, reminder: !task.reminder} : task
+    ))
+  }
   
   return (
     <div className="container"> 
-      <Header />
-      <Tasks tasks={tasks}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd= {showAddTask}/>
+      { showAddTask && <AddTask onAdd={addTask}/>}
+      {(tasks.length>0) ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No tasks for today"}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,17 @@ import PropTypes from 'prop-types' //rende il codice piu' robust in quanto defin
 import Button from './Button'
 
 
-export const Header = ({title}) => {
-    const onClick =() => {
-        console.log('Click')
-    }
+export const Header = ({title, onAdd, showAdd}) => {
+    // const onClick =() => {
+    //     console.log('Click')
+    // }
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <Button 
+                color={showAdd ? "red":"green"} 
+                text={showAdd ? "Close":"Add"} 
+                onClick={onAdd}/>
         </header>
     )
 }
